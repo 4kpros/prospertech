459468e76769eede0f5ab30ab8a8fb1e6f42b0e8
4kpros <prosper.abouar@gmail.com>
2021-06-24 22:59:22 +0100

    FR Update

--- static/custom/js/script.js
+++ static/custom/js/script.js
@@ -10,139 +10,139 @@ ScrollReveal().reveal('.my-section__container', { delay: 10, origin:'top', dista
 var galleryFluidMusicApp = [
     { 
         src: "https://docs.google.com/uc?export=download&id=10GTtHO3pZV_S8VynNQIHZQHCyUtcpKOw",
-        title: "Music Player Mobile App (travail en progression)",
+        title: "Music Player Mobile App (developpement en cours)",
         description: "Par ABOUAR PROSPER",
         theme: currentTheme 
     },
     { 
         src: "https://docs.google.com/uc?export=download&id=1yGySI2XSQ_NbUZdr13Zp-Po8nYHZry-z",
-        title: "Music Player Mobile App (travail en progression)",
+        title: "Music Player Mobile App (developpement en cours)",
         description: "Par ABOUAR PROSPER",
-        theme: currentTheme 
+        theme: currentTheme
     },
     { 
         src: "https://docs.google.com/uc?export=download&id=1gVucLiz81cbZLEYjmdm5F2eWdfS9hIU9",
-        title: "Music Player Mobile App (travail en progression)",
+        title: "Music Player Mobile App (developpement en cours)",
         description: "Par ABOUAR PROSPER",
-        theme: currentTheme 
+        theme: currentTheme
     },
     { 
         src: "https://docs.google.com/uc?export=download&id=1yGO99PStEkKmwRL84Gk7XR4IIQXbIEAF",
-        title: "Music Player Mobile App (travail en progression)",
+        title: "Music Player Mobile App (developpement en cours)",
         description: "Par ABOUAR PROSPER",
-        theme: currentTheme 
+        theme: currentTheme
     },
     { 
         src: "https://docs.google.com/uc?export=download&id=1Kp_eQzI9aJITpcLUU4wkpUDQtyq7lo7d",
-        title: "Music Player Mobile App (travail en progression)",
+        title: "Music Player Mobile App (developpement en cours)",
         description: "Par ABOUAR PROSPER",
-        theme: currentTheme 
+        theme: currentTheme
     }
 ];
 var galleryCovidApp = [
     {
         src: "https://docs.google.com/uc?export=download&id=1jvnU8zmzVKP1TGifHlLRxWCLzax13GOo",
-        title: "Fire Fighter Mobile App (mis en pause)",
+        title: "Firefighter mobile app (stoped)",
         description: "Par ABOUAR PROSPER, DONALD & SIME EXCEL",
-        theme: currentTheme,
+        theme: currentTheme
     },
     {
         src: "https://docs.google.com/uc?export=download&id=1icv8EUFl701AHM5b_WuShDntjU_JMYPB",
-        title: "Fire Fighter Mobile App (mis en pause)",
+        title: "Firefighter mobile app (stoped)",
         description: "Par ABOUAR PROSPER, DONALD & SIME EXCEL",
-        theme: currentTheme,
+        theme: currentTheme
     }
 ];
 var galleryFireFighterApp = [
     {
         src: "https://docs.google.com/uc?export=download&id=1772Aw6a3X44iqilN2PHop9lYz8TN9TLO",
-        title: "Fire Fighter Mobile App (travail en progression)",
-        description: "Par ABOUAR PROSPER & camarades en Master 1",
-        theme: currentTheme,
+        title: "Firefighter mobile app (developpement en cours)",
+        description: "Par ABOUAR PROSPER & membres du groupe",
+        theme: currentTheme
     },
     { 
         src: "https://docs.google.com/uc?export=download&id=1ueyHWOzBqJRHuiL39LCcVZQDSd6LJXDY",
-        title: "Fire Fighter Mobile App (travail en progression)",
-        description: "Par ABOUAR PROSPER & camarades en Master 1",
-        theme: currentTheme 
+        title: "Firefighter mobile app (developpement en cours)",
+        description: "Par ABOUAR PROSPER & membres du groupe",
+        theme: currentTheme
     },
     { 
         src: "https://docs.google.com/uc?export=download&id=1Yt-Y9GfzAfVuAhlPxwf3_7M12swQ_a4c",
-        title: "Fire Fighter Mobile App (travail en progression)",
-        description: "Par ABOUAR PROSPER & camarades en Master 1",
-        theme: currentTheme 
+        title: "Firefighter mobile app (developpement en cours)",
+        description: "Par ABOUAR PROSPER & membres du groupe",
+        theme: currentTheme
     },
     { 
         src: "https://docs.google.com/uc?export=download&id=1UnnBmS7hbbxVlzdqJD2uU0LBRzxMiGe0",
-        title: "Fire Fighter Mobile App (travail en progression)",
-        description: "Par ABOUAR PROSPER & camarades en Master 1",
-        theme: currentTheme 
+        title: "Firefighter mobile app (developpement en cours)",
+        description: "Par ABOUAR PROSPER & membres du groupe",
+        theme: currentTheme
     },
     { 
-        src: "https://docs.google.com/uc?export=download&id=1sGZnrs32XNbauiWq21lAnsS7-LPardzMZ",
-        title: "Fire Fighter Mobile App (travail en progression)",
-        description: "Par ABOUAR PROSPER & camarades en Master 1",
-        theme: currentTheme 
+        src: "https://docs.google.com/uc?export=download&id=1sGZnrs32XNbauiWq21lAnsS7-LbYdzMZ",
+        title: "Firefighter mobile app (developpement en cours)",
+        description: "Par ABOUAR PROSPER & membres du groupe",
+        theme: currentTheme
     },
     { 
         src: "https://docs.google.com/uc?export=download&id=1PckIZZCmEfSg9VYpuPoBN3_hSwjaj3ow",
-        title: "Fire Fighter Mobile App (travail en progression)",
-        description: "Par ABOUAR PROSPER & camarades en Master 1",
-        theme: currentTheme 
+        title: "Firefighter mobile app (developpement en cours)",
+        description: "Par ABOUAR PROSPER & membres du groupe",
+        theme: currentTheme
     },
     { 
         src: "https://docs.google.com/uc?export=download&id=1-cdxAZ_IMryk4B7CFlvw4-u2sTIYLy96",
-        title: "Fire Fighter Mobile App (travail en progression)",
-        description: "Par ABOUAR PROSPER & camarades en Master 1",
+        title: "Firefighter mobile app (developpement en cours)",
+        description: "Par ABOUAR PROSPER & membres du groupe",
         theme: currentTheme
     },
     { 
         src: "https://docs.google.com/uc?export=download&id=1FMwA6io_brVuhhpE-pRaGTqaXLdnQCXk",
-        title: "Fire Fighter Mobile App (travail en progression)",
-        description: "Par ABOUAR PROSPER & camarades en Master 1",
-        theme: currentTheme 
+        title: "Firefighter mobile app (developpement en cours)",
+        description: "Par ABOUAR PROSPER & membres du groupe",
+        theme: currentTheme
     },
     { 
         src: "https://docs.google.com/uc?export=download&id=1gVpZY2K2OVjFGLeG2uMXjf5ZMXo6Hz6m",
-        title: "Fire Fighter Mobile App (travail en progression)",
-        description: "Par ABOUAR PROSPER & camarades en Master 1",
-        theme: currentTheme 
+        title: "Firefighter mobile app (developpement en cours)",
+        description: "Par ABOUAR PROSPER & membres du groupe",
+        theme: currentTheme
     },
     { 
         src: "https://docs.google.com/uc?export=download&id=1oQcAOywEz5Z7nMxynX4VfGKE6O_UWHDC",
-        title: "Fire Fighter Mobile App (travail en progression)",
-        description: "Par ABOUAR PROSPER & camarades en Master 1",
-        theme: currentTheme 
+        title: "Firefighter mobile app (developpement en cours)",
+        description: "Par ABOUAR PROSPER & membres du groupe",
+        theme: currentTheme
     },
     { 
         src: "https://docs.google.com/uc?export=download&id=1HM603TEkJPcEc7Rsj_DpofEXYpcT5Id_",
-        title: "Fire Fighter Mobile App (travail en progression)",
-        description: "Par ABOUAR PROSPER & camarades en Master 1",
-        theme: currentTheme 
+        title: "Firefighter mobile app (developpement en cours)",
+        description: "Par ABOUAR PROSPER & membres du groupe",
+        theme: currentTheme
     },
     { 
         src: "https://docs.google.com/uc?export=download&id=1FLUyo6fMquzt3hQaLpuOK0TrIMSA5Fbl",
-        title: "Fire Fighter Mobile App (travail en progression)",
-        description: "Par ABOUAR PROSPER & camarades en Master 1",
+        title: "Firefighter mobile app (developpement en cours)",
+        description: "Par ABOUAR PROSPER & membres du groupe",
         theme: currentTheme
     },
     { 
         src: "https://docs.google.com/uc?export=download&id=1gVP9QZmjqPm4PCN4ZFcny8kpeGYsZh9-",
-        title: "Fire Fighter Mobile App (travail en progression)",
-        description: "Par ABOUAR PROSPER & camarades en Master 1",
-        theme: currentTheme 
+        title: "Firefighter mobile app (developpement en cours)",
+        description: "Par ABOUAR PROSPER & membres du groupe",
+        theme: currentTheme
     },
     { 
         src: "https://docs.google.com/uc?export=download&id=1azUpJQs0uaWYgRn_1UpHvuLThG-npefh",
-        title: "Fire Fighter Mobile App (travail en progression)",
-        description: "Par ABOUAR PROSPER & camarades en Master 1",
+        title: "Firefighter mobile app (developpement en cours)",
+        description: "Par ABOUAR PROSPER & membres du groupe",
         theme: currentTheme
     }
 ];
 //Web apps gallery
 var galleryCovidWeb = [
     {
-        src: "https://docs.google.com/uc?export=download&id=1ybzAiVgF3ParPev2Btz_xU8RXWR7BsGt-",
+        src: "https://docs.google.com/uc?export=download&id=1ybzAiVgF3ByPev2Btz_xU8RXWR7BsGt-",
         title: "Covid Stats Web App",
         description: "Par ABOUAR PROSPER, DONALD & SIME EXCEL",
         theme: currentTheme 
@@ -182,19 +182,19 @@ var galleryCovidWeb = [
 var galleryHouseUI = [
     {
         src: "https://docs.google.com/uc?export=download&id=19e-o69kNRpo49Gf7XdEhOu6zqLJNqlbP",
-        title: "Property for rent Mobile App - Prototype (Adobe XD)",
+        title: "Property rent - Prototype (Adobe XD)",
         description: "Par ABOUAR PROSPER",
         theme: currentTheme 
     },
     {
         src: "https://docs.google.com/uc?export=download&id=19-okyL6iMuNJYdBGhn_IkCSmED7WBSm1",
-        title: "Property for rent Mobile App - Prototype (Adobe XD)",
+        title: "Property rent - Prototype (Adobe XD)",
         description: "Par ABOUAR PROSPER",
         theme: currentTheme 
     },
     {
         src: "https://docs.google.com/uc?export=download&id=1btbEjbbwGGvEiwgGqF3IH0kWG2U3bB6x",
-        title: "Property for rent Mobile App - Prototype (Adobe XD)",
+        title: "Property rent - Prototype (Adobe XD)",
         description: "Par ABOUAR PROSPER",
         theme: currentTheme 
     }
@@ -203,64 +203,64 @@ var galleryHouseUI = [
 var galleryGameDevlog = [
     { 
         src: "https://docs.google.com/uc?export=download&id=1CTcU33Hsm1KEEIq69NTqA6MAIrfHG8x_",
-        title: "Game Devlogs (travail en progression)",
+        title: "Game Devlogs (developpement en cours)",
         description: "Par ABOUAR PROSPER",
         theme: currentTheme
     },
     { 
         src: "https://docs.google.com/uc?export=download&id=1nWceoP8T2W5UXaHLOrN3C7PEU6i1eqw8",
-        title: "Game Devlogs (travail en progression)",
+        title: "Game Devlogs (developpement en cours)",
         description: "Par ABOUAR PROSPER",
         theme: currentTheme
     },
     { 
         src: "https://docs.google.com/uc?export=download&id=1TTL8HSqhuWWx_k68WulsLIiRyEUh4Oe-",
-        title: "Game Devlogs (travail en progression)",
+        title: "Game Devlogs (developpement en cours)",
         description: "Par ABOUAR PROSPER",
         theme: currentTheme
     },
     { 
         src: "https://docs.google.com/uc?export=download&id=1uv9ymXI_RDco3kWVCCLi8Dfd_S1LrQf-",
-        title: "Game Devlogs (travail en progression)",
+        title: "Game Devlogs (developpement en cours)",
         description: "Par ABOUAR PROSPER",
         theme: currentTheme
     },
     { 
         src: "https://docs.google.com/uc?export=download&id=1YwMKE1JVnAWedjp1WQfh1VAgZqmZH549",
-        title: "Game Devlogs (travail en progression)",
+        title: "Game Devlogs (developpement en cours)",
         description: "Par ABOUAR PROSPER",
         theme: currentTheme
     },
     { 
         src: "https://docs.google.com/uc?export=download&id=187ZWCPOe8wW8kxpC0n5DxChmBeJgW6bn",
-        title: "Game Devlogs (travail en progression)",
+        title: "Game Devlogs (developpement en cours)",
         description: "Par ABOUAR PROSPER",
         theme: currentTheme
     },
     { 
         src: "https://docs.google.com/uc?export=download&id=1y8VoCDhtyQ9YJxhanDyWAOOW7yJDEq1O",
-        title: "Game Devlogs (travail en progression)",
+        title: "Game Devlogs (developpement en cours)",
         description: "Par ABOUAR PROSPER",
         theme: currentTheme
     },
     { 
         src: "https://docs.google.com/uc?export=download&id=1vr5T8W7Dy6ecXVnS-8_sqhC73_V9dPEc",
-        title: "Game Devlogs (travail en progression)",
+        title: "Game Devlogs (developpement en cours)",
         description: "Par ABOUAR PROSPER",
         theme: currentTheme
     },
     { 
         src: "https://docs.google.com/uc?export=download&id=1viIvHJpQkpjISGDdLaxN1iwbTcz8yUsj",
-        title: "Game Devlogs (travail en progression)",
+        title: "Game Devlogs (developpement en cours)",
         description: "Par ABOUAR PROSPER",
         theme: currentTheme
     }
 ];
 
 //For works filter
-const filterItems = document.getElementsParClassName("my__portfolio__item");
-const filterCards = document.getElementsParClassName("my__work__card__container");
-const workCardButtonMores = document.getElementsParClassName("my__work__item__look_text");
+const filterItems = document.getElementsByClassName("my__portfolio__item");
+const filterCards = document.getElementsByClassName("my__work__card__container");
+const workCardButtonMores = document.getElementsByClassName("my__work__item__look_text");
 
 var getItemWorkDetailsClicked = function(){
     var attribute = this.getAttribute("data-name");
@@ -326,7 +326,7 @@ for (var i = 0; i < filterItems.length; i++) {
     filterItems[i].addEventListener('click', getItemClicked, false);
 }
 
-var buttonTheme = document.getElementParId("my__nav__theme__toggle");
+var buttonTheme = document.getElementById("my__nav__theme__toggle");
 var currentTheme = "black"
 //Change the current theme
 var changeCurrentTheme = function(){
